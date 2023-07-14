9acf57e84fe3d55413f92e615efe2a42a3f4b795
Davy <davyd.dabahian@cheetahdigital.com>
2023-07-14 10:35:48 +0300

    материалы предыдущих занятий

--- summary_05/scripts/main.js
+++ summary_05/scripts/main.js
@@ -43,7 +43,7 @@ const shark = {
 };
 
 // Массив объектов
-let products = [ball, gloves, shoes, hammer, saw];
+//let products = [ball, gloves, shoes, hammer, saw];
 
 const productsContainer = document.querySelector('.products-container')
 function renderProduct(product) {
@@ -73,3 +73,131 @@ function renderProduct(product) {
     // добавляем наш товар в контейнер
     productsContainer.append(productContainer);
 }
+
+
+/* дан массив numbers. Посчитать сумму элементов массива, результат вывести в консоль. */
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+// console.log(sum);
+
+// let result = numbers.reduce( function(acc, val) {
+//     return acc + val;
+// }, 0);
+// console.log(result);
+
+/* вывести в консоль только четные элементы массива numbers (те, которые делятся на 2 без остатка)*/
+// const res = numbers.forEach( function(num, idx, thisArg) {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// });
+// console.log('result', res)
+
+// numbers.forEach( (num) => {
+//     if (num % 2 === 0) {
+//         console.log(num);
+//     }
+// })
+
+/* вывести в консоль только те элементы массива numbers, которые делятся без остатка на 2 и на 3.*/
+// numbers.forEach( (num) => {
+//     if ( (num % 2 === 0) && (num % 3 === 0)) {
+//         console.log(num);
+//     }
+// })
+
+// - добавить в массив numbers число 11 (в конец).
+// const res = numbers.push(11, 12, 13);
+// console.log(numbers, res);
+
+// - убрать из массива numbers число 11 (из конца).
+// const res = numbers.pop();
+// console.log(res, numbers)
+
+// - переместить первый элемент массива numbers в конец массива.
+// numbers.push(numbers.shift());
+// console.log(numbers);
+
+// - вернуть элемент обратно.
+// numbers.unshift(numbers.pop());
+// console.log(numbers);
+
+// - получить массив, который состоит из "среза" массива numbers, начиная с 3-го элемента.
+// const sliced = numbers.slice(2);
+// console.log(sliced);
+
+// ввыести в консоль массив в обратном порядке
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//     console.log(numbers[i]);
+// }
+// numbers.reverse();
+// console.log(numbers);
+
+/* вставить число 999 на 3-ю позицию в массив (т.е. numbers[2] = 999). Все последующие элементы должны "сдвинуться".
+*/
+// numbers.splice(2, 0, 999);
+// console.log(numbers)
+
+// - получить массив twiceNumbers, который будет состоять из элементов массива numbers, помноженных на 2. То есть, первый элемент массива twiceNumbers равен 2 * первый элемент массива numbers, и т.д.
+// const twiceNumbers = numbers.map( (item) => {
+//     return item * 2;
+// });
+
+// console.log(twiceNumbers);
+
+//  получить массив squares, который будет состоять из квадратов чисел массива numbers.
+// const squares = numbers.map( (item) => {
+//     return item * item; // Math.pow(item, 2) // item ** 2;
+// });
+// console.log(squares);
+
+/*
+получить из массива products массив, который будет содержать только наименования товаров (ожидаемый результат - `['Ball', 'Gloves', 'Sport Shoes', 'Hammer', 'Saw', 'Shark toy']`)
+*/
+let products = [ball, gloves, shoes, hammer, saw];
+// const productNames = products.map( (item) => {
+//     return item.name;
+// });
+// productNames = [];
+// 1-st iteration
+// item == ball
+// returns ball.name - Ball
+// productNames = ['Ball']
+// 2-nd iteration
+// item == gloves
+// returns gloves.name - Gloves
+// productNames = ['Ball', 'Gloves']
+// ...
+// console.log(productNames)
+
+/*
+получить из массива products массив объектов, которые содержат только название и цену товара. То есть, объект товара "превращается" в объект, у которого есть только свойства name, price.
+*/
+// let newProducts = products.map( (item) => {
+//     const result = {};
+//     result.name = item.name;
+//     result.price = item.price;
+//     /**
+//      * result = {
+//      *   name: item.name,
+//      *   price: item.price
+//      * }
+//      */
+//     return result;
+// });
+
+// console.log(newProducts)
+
+// // получить массив evenNumbers, который состоит только из четных элементов массива numbers.
+// const evenNumbers = numbers.filter( (item) => {
+//     return item % 2 === 0;
+// });
+// console.log(evenNumbers);
+
+// получить массив newNumbers, который будет содержать только те элементы массива numbers, которые больше 4 и меньше 8.
+const newNumbers = numbers.filter( (item) => {
+    return item > 4 && item < 8;
+});
+console.log(newNumbers);
--- summary_06/scripts/main.js
+++ summary_06/scripts/main.js
@@ -3,6 +3,34 @@
 // Массив чисел (примитивы)
 const numbers = [1,3,6,20,4,0,9,7,5,10,15,12,11,50,24,32,7,30];
 
+// const sum = numbers.reduce( (acc, val) => {
+//     if (val % 2 === 0) {
+//         acc = acc + val;
+//     }
+//     return acc;
+// }, 0);
+// console.log(sum);
+
+
+/*
+1. Повторение работы с массивами. Работаем с разными методами массивов.
+ - посчитать сумму элементов массива numbers. Сделаем это двумя способами:
+    - через перебор массива (`forEach`)
+    - с использованием метода массивов `reduce`
+*/
+// const result = numbers.reduce( (counter, item) => {
+//     return counter + item;
+// }, 0);
+// acc = 0, val = 1
+// The end of 1st iteration: acc = 1, val = 1
+// The start of 2nd iteration: acc = 1, val = 3
+// The end of 2nd iteration: acc = 4, val = 3
+// The start of 3rd iteation: acc = 4, val = 6
+// The end of 3rd iteration: acc = 10, val = 6
+//...
+
+// console.log(result);
+
 const ball = {
     name: "Ball",
     category: "Sport",
@@ -45,7 +73,61 @@ const shark = {
 // Массив объектов
 let products = [ball, gloves, shoes, hammer, saw, shark];
 
+// const sumProducts = products.reduce( (acc, product) => {
+//     return acc + product.price;
+// }, 0);
+// console.log(sumProducts);
+
+/*
+ - получить массив категорий товаров (т.е. нужно пройтись по массиву товаров методом reduce, и добавлять в "аккумулятор" свойство category рассматриваемого товара)
+ */
+// let categories = products.reduce( ( acc, product ) => {
+//     if (acc.indexOf(product.category) === -1) {
+//         acc.push(product.category);
+//     }
+//     return acc;
+// }, []);
+// console.log(categories)
+// Start of 1st iteration: acc = [], product = ball
+// End of the 1st iteration: acc = ['Sport'], product = ball
+// Start of 2nd iteration: acc = ['Sport'], product = gloves
+// End of 2nd iteration: acc = ['Sport'], product = gloves
+// ...
+// Start of 4rd iteration: acc = ['Sport'], product = hammer
+// End of 4th iteration: acc = ['Sport', 'Tools'], product = hammer
+// const res = numbers.reduce( (acc, val) => acc + Math.pow(val,2), 0);
+// console.log(res);
+
+// const strings = ['Str1', 'Product', 'Timon', 'Simba', 'Pumba', 'Accumulator'];
+
+// const res1 = strings.reduce ((counter, item) => {
+//     return counter + item.length;
+// }, 0)
+// console.log(res1);
+
+/* посчитать сумму четных элементов массива numbers c использованием метода reduce */
+
+
+/*
+ - получить сумму квадратов массива numbers
+ - получить суммарную стоимость всех товаров массива products
+ - получить массив категорий товаров (т.е. нужно пройтись по массиву товаров методом reduce, и добавлять в "аккумулятор" свойство category рассматриваемого товара)
+ - получить объект, в котором будут категории товаров.
+ - получить объект, в котором будут суммарные стоимости товаров по категориям.
+*/
+
+
 const productsContainer = document.querySelector('.products-container')
+
+const selectEl = document.querySelector('#categorySelect');
+const options = selectEl.querySelectorAll('option');
+
+const newParagraph = document.createElement('p');
+newParagraph.innerText = 'Hello World!';
+console.dir(newParagraph);
+
+productsContainer.before(newParagraph);
+
 function renderProduct(product) {
     const productContainer = document.createElement('div');
     productContainer.dataset.name = product.name;
@@ -73,6 +155,19 @@ function renderProduct(product) {
     // добавляем наш товар в контейнер
     productsContainer.append(productContainer);
 }
+
+products.forEach( (product) => {
+    renderProduct(product);
+})
+
+// Записать в эту переменную все параграфы (тег p) с классом product-name
+const productNames = document.querySelectorAll('p.product-name')
+
+console.log(productNames);
+
+/*
+9. Теперь отрендрим список товаров. У нас в main.js определена функция `renderProduct`. Она ожидает на вход объект товара, создает DOM-элемент, и добавляет его на страницу (в контейнер). У нас есть массив товаров `products`. Нужно отрендерить эти товары, вызовом функции `renderProduct` для каждого элемента массива `products`. Т.е. пройтись по массиву и вызвать функцию для каждого элемента.
+*/
 
 // Заготовка функции отрисовки массива товаров (для задачи 10)
 function renderProductsArray(productsArray) {
