20212dfe63293f461ab53e8473aefa6a16998770
Davy <davyd.dabahian@cheetahdigital.com>
2023-08-04 00:25:55 +0300

    Материалы 8-го занятия - работа с DOM

---
+++ summary_08/scripts/main.js
@@ -0,0 +1,41 @@
+const ball = {
+    name: "Ball",
+    category: "Sport",
+    img: "./assets/ball.jpeg",
+    price: 160
+};
+const gloves = {
+    name: "Gloves",
+    category: "Sport",
+    img: "./assets/gloves.jpg",
+    price: 140
+};
+const shoes = {
+    name: "Sport shoes",
+    category: "Sport",
+    discount: true,
+    img: "./assets/shoes.png",
+    price: 180
+};
+const hammer = {
+    name: "Hammer",
+    category: "Tools",
+    img: "./assets/hammer.jpeg",
+    price: 40
+};
+const saw = {
+    name: "Saw",
+    category: "Tools",
+    discount: true,
+    img: "./assets/saw.jpeg",
+    price: 210
+};
+const shark = {
+    name: "Shark toy",
+    category: "Other",
+    img: "./assets/shark.jpeg",
+    price: 45
+};
+
+// Массив объектов
+let products = [ball, gloves, shoes, hammer, saw, shark];
