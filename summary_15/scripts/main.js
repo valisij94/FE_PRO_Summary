ea48ba84c022d0014a2e19c1850ba752b38ad1cd
Davy <davyd.dabahian@cheetahdigital.com>
2023-09-21 23:57:13 +0300

    Материалы 15-го занятия - контекст

---
+++ summary_15/scripts/main.js
@@ -0,0 +1,44 @@
+class Product {
+
+    constructor(id, name, price, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
+
+class PowerTool extends Product {
+    constructor(id, name, price, description, power, warranty) {
+        super(id, name, price, description);
+        this.power = power;
+        this.warranty = warranty;
+    }
+}
+
+class Tool extends Product {
+    constructor(id, name, price, description, strength) {
+        super(id, name, price, description);
+        this.strength = strength;
+    }
+}
+
+const weirdArray = {
+    length: 2,
+    0: 'Saw',
+    1: 'Hammer'
+}
+
+const veryWeirdArray = {
+    0: 'Drill',
+    1: 'Axe',
+    2: 'Nails',
+};
+
+function customMap(callback) {
+    let result = [];
+    for (let i = 0; i < this.length; i++) {
+        result[i] = callback(this[i]);
+    }
+    return result;
+};
